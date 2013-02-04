0fda047f46b4a39eb7dbe916ce752b81365389cf
KDr2 <killy.draw@gmail.com>
2013-02-04 15:52:21 +0800

    starting python api

---
+++ src/python_api.cpp
@@ -0,0 +1,23 @@
+/*
+ * python module HQL :
+ *   Haddit Query Language
+ *
+ * author : KDr2
+ *
+ */
+
+
+#include "ast.hpp"
+#include "ast_util.hpp"
+#include "cmd.hpp"
+#include "parser.hpp"
+#include "type_config.hpp"
+#include "trollers.hpp"
+#include "cmn_util.hpp"
+
+#include <iostream>
+#include <fstream>
+
+using std::ifstream;
+
+//TODO
